4783f4c6cca5a821eff9767c01f60922d303f856
lharti <7027812+lharti@users.noreply.github.com>
2024-12-26 16:24:04 +0100

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
 import js from '@eslint/js'
+import reactPlugin from 'eslint-plugin-react'
+import reactCompiler from 'eslint-plugin-react-compiler'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginReadableTailwind from 'eslint-plugin-readable-tailwind'
+import tailwind from 'eslint-plugin-tailwindcss'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
@@ -8,7 +12,15 @@ export default tseslint.config(
     { ignores: ['dist'] },
 
     {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+
+            ...tailwind.configs['flat/recommended'],
+
+            reactPlugin.configs.flat.recommended,
+            reactPlugin.configs.flat['jsx-runtime'],
+        ],
 
         files: ['**/*.{ts,tsx}'],
 
@@ -20,14 +32,51 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'react-compiler': reactCompiler,
+            'readable-tailwind': eslintPluginReadableTailwind,
         },
 
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': [
                 'warn',
-                { allowConstantExport: true },
+                {
+                    allowConstantExport: true,
+                },
             ],
+
+            'react-compiler/react-compiler': 'error',
+
+            ...eslintPluginReadableTailwind.configs.error.rules,
+
+            'react/jsx-filename-extension': [
+                'warn',
+                {
+                    extensions: ['.tsx'],
+                },
+            ],
+
+            'react/jsx-sort-props': [
+                'warn',
+                {
+                    multiline: 'last',
+                    shorthandFirst: true,
+                    noSortAlphabetically: true,
+                    callbacksLast: true,
+                },
+            ],
+
+            'react/function-component-definition': [
+                'warn',
+                {
+                    namedComponents: 'arrow-function',
+                    unnamedComponents: 'arrow-function',
+                },
+            ],
+
+            'react/prop-types': 'off',
+
+            'require-await': 'warn',
         },
     },
 )
