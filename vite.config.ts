52ba99cf08a2eadeed4a41c83ee3398a0eca3028
lharti <7027812+lharti@users.noreply.github.com>
2024-12-26 16:07:20 +0100

    build(vite): optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,46 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+const ReactCompilerConfig = {
+    target: '19',
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+            },
+        }),
+    ],
 
     resolve: {
         alias: {
             '@': '/src',
+        },
+    },
+
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom'],
+                    transformers: ['@huggingface/transformers'],
+                },
+            },
+        },
+
+        chunkSizeWarningLimit: 1500,
+    },
+
+    optimizeDeps: {
+        exclude: ['@huggingface/transformers'],
+    },
+
+    server: {
+        watch: {
+            ignored: ['**/*.{spec,test}.ts(x)?', 'coverage'],
         },
     },
 })
