479e9f66cced17805c4d281636f253b84a6d3a53
lharti <7027812+lharti@users.noreply.github.com>
2024-12-26 16:28:41 +0100

    feat: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { BgRemover } from '@/components/BgRemover'
+import { RmbgModelLoader } from '@/components/RmbgModelLoader'
+import { DashPage } from '@/pages/Dash'
+import { HomePage } from '@/pages/Home'
+import { ImageItem } from '@/types/imageItem'
+import { useState } from 'react'
 
-function App() {
-    return <BgRemover className="mx-auto mt-20 max-w-screen-lg px-6" />
+const App = () => {
+    const [initialImages, setInitialImages] = useState<ImageItem[] | null>(null)
+
+    return (
+        <>
+            {initialImages ? (
+                <DashPage
+                    className={`mx-auto mt-20 max-w-screen-lg px-6`}
+                    initialImages={initialImages}
+                />
+            ) : (
+                <HomePage onImagesSelect={images => setInitialImages(images)} />
+            )}
+
+            <RmbgModelLoader />
+        </>
+    )
 }
 
 export default App
